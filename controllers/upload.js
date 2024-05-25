7f8534028ed8201cdbb79d000d4bb09a83296074
v-Sharan <vsharansago@gmail.com>
2024-05-25 14:24:46 +0530

    console

--- controllers/upload.js
+++ controllers/upload.js
@@ -4,10 +4,10 @@ export const ImageText = async (req, res, next) => {
   const input = {
     image: req.file.path,
   };
-  const output = await replicateClient.run(
-    "salesforce/blip:2e1dddc8621f72155f24cf2e0adbde548458d3cab9f00c0139eea840d0ac4746",
-    { input }
-  );
-  console.log(output);
-  res.json({ message: output });
+  //   const output = await replicateClient.run(
+  //     "salesforce/blip:2e1dddc8621f72155f24cf2e0adbde548458d3cab9f00c0139eea840d0ac4746",
+  //     { input }
+  //   );
+  console.log(input.image);
+  res.json({ message: "Done" });
 };
